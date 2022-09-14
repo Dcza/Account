7732596178bbc5a557e57b82e03ca0c5332b6e33
Daniel <TestowyMail@git.pl>
2022-09-14 23:23:23 +0200

    Added TestAccount class and few additional test functions

--- main.py
+++ main.py
@@ -10,30 +10,55 @@ class Account:
         return self.balance
 
     def deposit(self, amount):
-        self.balance += amount
+        if amount < 0:
+            print("Operation Aborted: You cannot deposit negative value! Balance on your account has not been changed")
+        else:
+            self.balance += amount
 
     def withdraw(self, amount):
-        self.balance -= amount
+        if amount < 0:
+            print("Operation Aborted: You cannot withdraw negative value! Balance on your account has not been changed")
+        else:
+            self.balance -= amount
 
 
 a1 = Account("Jan Kowalski", 1000)
 
 
-def test_get_owner():
-    assert isinstance(a1.get_owner(), str)
+class TestAccount:
+    def setup(self):
+        # Given (part1)
+        self.owner = "Jan Kowalski"
+        self.account = Account(self.owner, 1000)
 
+    def test_get_owner(self):  # Check if owner is in string type and if it's return same value as we provided
+        assert isinstance(self.account.get_owner(), str)
+        assert self.account.get_owner() == self.owner
 
-def test_get_balance():
-    assert isinstance(a1.get_balance(), (int, float))
+    def test_get_balance(self):
+        assert isinstance(self.account.get_balance(), int)
 
+    def test_deposit(self):
+        orig_balance = self.account.get_balance()
+        self.account.deposit(100)
+        assert self.account.get_balance() == orig_balance + 100
 
-def test_deposit():
-    balance = a1.balance
-    a1.deposit(100)
-    assert a1.balance - balance == 100
+    def test_withdraw(self):
+        orig_balance = self.account.get_balance()
+        self.account.withdraw(100)
+        assert self.account.get_balance() == orig_balance - 100
 
+    def test_deposit_negative(self):
+        orig_balance = self.account.get_balance()
+        self.account.deposit(-100)
+        assert self.account.get_balance() == orig_balance
 
-def test_withdraw():
-    balance = a1.balance
-    a1.withdraw(100)
-    assert balance - a1.balance == 100
+    def test_withdraw_negative(self):
+        orig_balance = self.account.get_balance()
+        self.account.withdraw(-100)
+        assert self.account.get_balance() == orig_balance
+
+    def test_deposit_huge(self):
+        orig_balance = self.account.get_balance()
+        self.account.deposit(156151651651655)
+        assert self.account.get_balance() == orig_balance + 156151651651655
